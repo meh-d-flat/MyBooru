c1f8eda183f2cea14020689d6dfebaf125b1f539
meh-d-flat <31995784+meh-d-flat@users.noreply.github.com>
2022-04-22 23:10:45 +0300

    request limit

---
+++ MyBooru/IPAddressRecord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace MyBooru
+{
+    public class IPAddressRecord
+    {
+        public IPAddress RemoteIP { get; }
+        public IPAddress LocalIP { get; }
+        public DateTime InitialRequestTime { get; }
+        public DateTime LastRequestTime { get; set; }
+        public int NumberOfRequests { get; set; }
+
+        public IPAddressRecord(IPAddress remote, IPAddress local, DateTime initial)
+        {
+            RemoteIP = remote;
+            LocalIP = local;
+            InitialRequestTime = initial;
+        }
+
+        public override string ToString()
+        {
+            return $"remote|local : {RemoteIP}|{LocalIP}\ninitial|last|amount : {InitialRequestTime}|{LastRequestTime}|{NumberOfRequests}";
+        }
+    }
+}
